d32c4121eb985606a53ca20416b5ef105702e702
ML <54767542+malijuly@users.noreply.github.com>
2020-04-17 21:03:14 +0200

    Add files via upload

---
+++ brickBracker/src/brickBracker/Main.java
@@ -0,0 +1,23 @@
+package brickBracker;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		JFrame obj = new JFrame();
+		GamePlay gamePlay = new GamePlay();
+		
+		//propierties of frame
+		obj.setBounds(10, 10, 710, 600);
+		obj.setTitle("Breakout Ball");
+		obj.setResizable(false);
+		obj.setVisible(true);
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		obj.add(gamePlay);
+		
+
+	}
+
+}
